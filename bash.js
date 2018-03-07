1a7cdd9392d5187fb2e9c94737c67b891c4cc06d
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-03-07 14:03:46 -0500

    .gitignore

--- bash.js
+++ bash.js
@@ -1,15 +1,18 @@
-var commands = require('./commands.js');
+var commands = require("./commands.js")
 
-process.stdout.write('prompt: ');
+process.stdout.write("prompt: ")
 
-process.stdin.on('data', function(data) {
-  var input = data.toString().trim().split(' ');
-  cmd = input[0];
-  args = input.slice(1).join(' ');
-  commands[cmd](args);
-  setTimeout(function(){
-    process.stdout.write('\nprompt: ');
-  }, 500);
+process.stdin.on("data", function(data) {
+  var input = data
+      .toString()
+      .trim()
+      .split(" "),
+    cmd = input[0],
+    args = input.slice(1).join(" ")
+  commands[cmd](args)
+  setTimeout(function() {
+    process.stdout.write("\nprompt: ")
+  }, 500)
   // process.stdout.write('You typed: ' + cmd);
   // process.stdout.write('You typed: ' + cmd);
 })
--- commands.js
+++ commands.js
@@ -1,91 +1,103 @@
-var fs = require('fs');
-var request = require('request');
+var fs = require("fs")
+var request = require("request")
 
-var done = function(output){
-  process.stdout.write(output);
+var done = function(output) {
+  process.stdout.write(output)
 }
 
 module.exports = {
-  pwd: function(){
-    done(process.cwd());
+  pwd: function() {
+    done(process.cwd())
   },
-  date: function(){
-    var date = new Date();
-    done(date.toString());
+  date: function() {
+    var date = new Date()
+    done(date.toString())
   },
-  ls: function(){
-    fs.readdir('.', function(err, filesArr){
-      if (err) throw err;
-      filesArr.forEach(function(file){
-        done(file.toString() + '\n');
+  ls: function() {
+    fs.readdir(".", function(err, filesArr) {
+      if (err) throw err
+      filesArr.forEach(function(file) {
+        done(file.toString() + "\n")
       })
     })
   },
-  echo: function(args){
-    if(args[0] === '$' && process.env[args.slice(1)]) {
-      done(process.env[args.slice(1)]);
+  echo: function(args) {
+    if (args[0] === "$" && process.env[args.slice(1)]) {
+      done(process.env[args.slice(1)])
     } else {
-      done(args);
+      done(args)
     }
   },
-  cat: function(args){
+  cat: function(args) {
     fs.readFile(args, (err, data) => {
-      if(err) throw err;
-      done(data);
+      if (err) throw err
+      done(data)
     })
   },
-  head: function(args){
+  head: function(args) {
     fs.readFile(args, (err, data) => {
-      var dataHead = data.toString().split(/\n/).slice(0, 5).join('\n');
-      if(err) throw err;
-      done(dataHead);
+      var dataHead = data
+        .toString()
+        .split(/\n/)
+        .slice(0, 5)
+        .join("\n")
+      if (err) throw err
+      done(dataHead)
     })
   },
-  tail: function(args){
+  tail: function(args) {
     fs.readFile(args, (err, data) => {
-      var dataTail = data.toString().split(/\n/).slice(-5).join('\n');
-      if(err) throw err;
-      done(dataTail);
+      var dataTail = data
+        .toString()
+        .split(/\n/)
+        .slice(-5)
+        .join("\n")
+      if (err) throw err
+      done(dataTail)
     })
   },
-  sort: function(args){
+  sort: function(args) {
     fs.readFile(args, (err, data) => {
-      var dataSorted = data.toString().split(/\n/).sort().join('\n');
-      if(err) throw err;
-      done(dataSorted);
+      var dataSorted = data
+        .toString()
+        .split(/\n/)
+        .sort()
+        .join("\n")
+      if (err) throw err
+      done(dataSorted)
     })
   },
-  wc: function(args){
+  wc: function(args) {
     fs.readFile(args, (err, data) => {
-      var dataLength = data.toString().split(/\n/).length.toString();
-      if(err) throw err;
-      done(dataLength);
+      var dataLength = data
+        .toString()
+        .split(/\n/)
+        .length.toString()
+      if (err) throw err
+      done(dataLength)
     })
   },
-  uniq: function(args){
+  uniq: function(args) {
     fs.readFile(args, (err, data) => {
-      var dataArr = data.toString().split(/\n/);
-      var result = dataArr.filter(function(value, index){
-        if(value !== dataArr[index-1]){
-          return true;
+      var dataArr = data.toString().split(/\n/)
+      var result = dataArr.filter(function(value, index) {
+        if (value !== dataArr[index - 1]) {
+          return true
         }
-        return false;
+        return false
       })
-      if(err) throw err;
-      done(result.join('\n'));
+      if (err) throw err
+      done(result.join("\n"))
     })
   },
-  curl: function(url){
-    var fileName = url.split('/');
-    var file = fileName[fileName.length-1];
+  curl: function(url) {
+    var fileName = url.split("/")
+    var file = fileName[fileName.length - 1]
     request
       .get(url)
-      .on('error', function(err){
-        done(err);
+      .on("error", function(err) {
+        done(err)
       })
-      .pipe(fs.createWriteStream(file));
+      .pipe(fs.createWriteStream(file))
   }
 }
-
-
-
